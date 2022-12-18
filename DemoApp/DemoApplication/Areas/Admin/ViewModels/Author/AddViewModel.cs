5689579bef8cabd7ecf86b918252ec257df8c9cb
Ceyhun <contact.ceyhun@gmail.com>
2022-12-18 17:49:10 +0400

    Everyting isnt done

---
+++ DemoApp/DemoApplication/Areas/Admin/ViewModels/Author/AddViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace DemoApplication.Areas.Admin.ViewModels.Author
+{
+    public class AddViewModel
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+
+
+
+        public AddViewModel()
+        {
+
+        }
+
+        public AddViewModel(int id, string name, string lastName)
+        {
+            Name = name;
+            LastName = lastName;
+        }
+      
+    }
+}
---
+++ DemoApp/DemoApplication/wwwroot/admin/assets/js/author.js
@@ -0,0 +1,15 @@
+﻿
+//$(document).on('click', ".add-author" , function (e) {
+//    var url = e.target.nextElementSibling.href;
+
+//    console.log("salam")
+
+
+//    fetch(url)
+//        .then(response => response.text())
+//        .then(data => {
+//            console.log(data)
+//            $('.cart-block').html(data);
+//        })
+//})
+
